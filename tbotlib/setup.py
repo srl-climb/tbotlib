ee5fff2f080848d6367eef0fb3c67b83db48f916
gbcarlos <carlos.giese@web.de>
2023-08-16 12:31:38 +0900

    Added dependencies to setup.py and gitignore file to ignore cache folders

--- tbotlib/setup.py
+++ tbotlib/setup.py
@@ -5,5 +5,15 @@ setup(
     version='0.1.0',
     author='Simon Harms',
     author_email='harms.simon759@mail.kyutech.jp',
-    packages=find_packages(include=['tbotlib', 'tbotlib.*'])
+    packages=find_packages(include=['tbotlib', 'tbotlib.*']),
+    install_requires=[
+        "qpsolvers ==3.4.0",
+        "numpy-quaternion ==3.5",
+        "screeninfo ==0.8.1",
+        "alphashape ==1.3.1",
+        "open3d ==0.13.0",
+        "numpy ==1.24",
+        "networkx ==3.1",
+        "matplotlib ==3.1.2"
+    ]
 )
