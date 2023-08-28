467bbaf595578eaecfc75f2061142eb0b5db53d5
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2023-08-28 16:46:10 +0900

    Removed print() function from ang2

--- tbotlib/tbotlib/tools/ang2.py
+++ tbotlib/tbotlib/tools/ang2.py
@@ -3,9 +3,6 @@ import numpy as np
 
 def ang2(u: np.ndarray, v: np.ndarray) -> float:
 
-
-    print(u@v / np.linalg.norm(u)/ np.linalg.norm(v))
-
     theta = np.arccos(np.clip( u@v /np.linalg.norm(u)/np.linalg.norm(v), -1, 1))
 
     return theta
