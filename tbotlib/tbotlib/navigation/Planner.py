25bdb968822d3dcbffb921605ab72db8f63a6dd6
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2024-05-24 11:28:02 +0900

    Added functionality to research the global graph if the local planning fails to GlobalPlanner plan method.

--- tbotlib/tbotlib/navigation/Planner.py
+++ tbotlib/tbotlib/navigation/Planner.py
@@ -55,7 +55,7 @@ class PlanPlatform2Pose(LocalPlanner):
         
         if commands is not None and exitflag:
             commands.append(CommandMovePlatform(profile))
-        
+
         if exitflag:
             tetherbot.platform.T_world = profile.poses[-1]
 
@@ -129,6 +129,8 @@ class PlanPlatform2Configuration(PlanPlatform2Pose):
         tetherbot.tension(grip_idx, False)
         stability, goal = self._workspace.calculate(tetherbot)
         tetherbot.tension(grip_idx, True)
+        
+        print(stability, goal)
 
         if stability > 0:
             tetherbot, profile, exitflag = super(PlanPlatform2Pose, self).plan(tetherbot, goal)
@@ -214,7 +216,7 @@ class PlanPickAndPlace(AbstractPlanner):
 
         # move platform into stable position
         tetherbot, commands, exitflag = self._platform2configuration.plan(tetherbot, grip_idx, commands)
-        
+ 
         if exitflag is None: return tetherbot, commands, False
 
         # move platform close to gripper
@@ -303,17 +305,18 @@ class PlanPickAndPlace2(PlanPickAndPlace):
         # 9:  place gripper
         # 10: move arm to gripper hoverpoint
 
+        commands_temp = commands
+        commands = CommandList()
+
         next_state = start_state
         while True:
             current_state = next_state
-
             if current_state == 0:
                 # move platform into stable position
                 tetherbot, commands, exitflag = self._platform2configuration.plan(tetherbot, grip_idx, commands)
                 # untension the tethers of the gripper
                 tetherbot.tension(grip_idx, False)
-                if commands is not None:
-                    commands.append(CommandTensionTethers(grip_idx, False))
+                commands.append(CommandTensionTethers(grip_idx, False))
                 next_state = 1
             elif current_state == 1:
                 # move platform close to gripper
@@ -332,8 +335,7 @@ class PlanPickAndPlace2(PlanPickAndPlace):
             elif current_state == 4:
                 # pick gripper
                 tetherbot.pick(grip_idx)
-                if commands is not None:
-                    commands.append(CommandPickGripper(grip_idx))
+                commands.append(CommandPickGripper(grip_idx))
                 next_state = 5
             elif current_state == 5:
                 # move arm with gripper to hoverpoint
@@ -347,35 +349,41 @@ class PlanPickAndPlace2(PlanPickAndPlace):
                 # move gripper with arm to hoverpoint
                 tetherbot, commands, exitflag = self._arm2pose.plan(
                     tetherbot, 
-                    TransformMatrix(tetherbot.wall.holds[hold_idx].hoverpoint.r_world + tetherbot.grippers[grip_idx].dockpoint.r_local - tetherbot.grippers[grip_idx].grippoint.r_local), 
+                    TransformMatrix(tetherbot.wall.holds[hold_idx].hoverpoint.r_world + tetherbot.grippers[grip_idx].dockpoint.r_world - tetherbot.grippers[grip_idx].grippoint.r_world), 
                     commands)
                 next_state = 8
             elif current_state == 8:
                 # move gripper with arm to grippoint
                 tetherbot, commands, exitflag = self._arm2pose.plan(
                     tetherbot,
-                    TransformMatrix(tetherbot.wall.holds[hold_idx].grippoint.r_world + tetherbot.grippers[grip_idx].dockpoint.r_local - tetherbot.grippers[grip_idx].grippoint.r_local), 
+                    TransformMatrix(tetherbot.wall.holds[hold_idx].grippoint.r_world + tetherbot.grippers[grip_idx].dockpoint.r_world - tetherbot.grippers[grip_idx].grippoint.r_world), 
                     commands)
                 next_state = 9
             elif current_state == 9:
                 # place gripper
                 tetherbot.place(grip_idx, hold_idx, correct_pose = True)
-                if commands is not None:
-                    commands.append(CommandPlaceGripper(grip_idx, hold_idx))
+                commands.append(CommandPlaceGripper(grip_idx, hold_idx))
                 next_state = 10
             elif current_state == 10:
                 # move arm to gripper hoverpoint
                 tetherbot, commands, exitflag = self._arm2pose.plan(tetherbot, tetherbot.grippers[grip_idx].hoverpoint.T_world, commands)
                 # tension the tethers of the gripper
                 tetherbot.tension(grip_idx, True)
-                if commands is not None:
-                    commands.append(CommandTensionTethers(grip_idx, True))
+                commands.append(CommandTensionTethers(grip_idx, True))
                 next_state = 0
+            
+            if exitflag is None or current_state == goal_state:
+                break
 
-            if exitflag is None: 
-                return tetherbot, commands, False
-            if current_state == goal_state:
-                return tetherbot, commands, True    
+        if exitflag is None: 
+            commands = commands_temp
+            exitflag = False
+        else:
+            if commands_temp is not None:
+                commands = CommandList(commands_temp + commands)
+            exitflag = True
+
+        return tetherbot, commands, exitflag
 
 class FastPlanPickAndPlace(AbstractPlanner):
 
@@ -435,7 +443,7 @@ class GlobalPlanner(AbstractPlanner):
             graph = TbGlobalGraph(goal_dist=0.001, planner=FastPlanPickAndPlace(), workspace=TbWorkspace(padding=[-0.1,-0.1,0,-180,-180,-135], scale=[0.3,0.3,0.1,45,45,45], mode = 'first'))
 
         if localplanner is None:
-            localplanner = PlanPickAndPlace()
+            localplanner = PlanPickAndPlace2()
 
         self._graph        = graph
         self._localplanner = localplanner
@@ -445,31 +453,121 @@ class GlobalPlanner(AbstractPlanner):
         print()
         print('global path planning...')
 
-        path = self._graph.search(tetherbot, tuple(start), tuple(goal))
+        command_cache: dict[tuple, tuple[CommandList, dict]] = {tuple(start): (commands, tetherbot.get_state())}
 
-        if path is not None:
+        while True:
+
+            print('global path planning...')
+
+            # calculate steps
+            path = self._graph.search(tetherbot, tuple(start), tuple(goal))
+
+            """ path = ClimbPath([[ 8,  2, 14,  0, 12],
+                              [-1,  2, 14,  0, 12],
+                              [15,  2, 14,  0, 12],
+                              [15, -1, 14,  0, 12],
+                              [15, 10, 14,  0, 12],
+                              [15, 10, 14, -1, 12],
+                              [15, 10, 14,  9, 12],
+                              [15, 10, 14,  9, -1],
+                              [15, 10, 14,  9,  8],
+                              [15, -1, 14,  9,  8],
+                              [15, 16, 14,  9,  8],
+                              [-1, 16, 14,  9,  8],
+                              [21, 16, 14,  9,  8],
+                              [21, -1, 14,  9,  8],
+                              [21, 22, 14,  9,  8],
+                              [21, 22, -1,  9,  8],
+                              [21, 22, 20,  9,  8],
+                              [21, 22, 20,  9, -1],
+                              [21, 22, 20,  9, 14],
+                              [21, 22, -1,  9, 14],
+                              [21, 22, 19,  9, 14],
+                              [-1, 22, 19,  9, 14],
+                              [28, 22, 19,  9, 14],
+                              [28, 22, 19, -1, 14],
+                              [28, 22, 19, 16, 14],
+                              [28, -1, 19, 16, 14],
+                              [28, 29, 19, 16, 14],
+                              [28, 29, 19, -1, 14],
+                              [28, 29, 19, 23, 14],
+                              [28, 29, -1, 23, 14],
+                              [28, 29, 27, 23, 14],
+                              [28, 29, 27, 23, -1],
+                              [28, 29, 27, 23, 21],
+                              [-1, 29, 27, 23, 21],
+                              [34, 29, 27, 23, 21],
+                              [34, -1, 27, 23, 21],
+                              [34, 35, 27, 23, 21],
+                              [34, 35, -1, 23, 21],
+                              [34, 35, 33, 23, 21]]) """
+            
+            if path is None:
+                exitflag = False
+                print('...failed.')
+                break
+
             print('...successful!')
             print(path.coordinates)
-            exitflag = True
-        else:
-            print('...failed.')
-            exitflag = False
-
-        if commands is not None and exitflag:
-
+            print()
             print('local path planning...')
 
-            for grip_idx, hold_idx in zip(path.grip_idc, path.hold_idc):
-                print('gripper ', grip_idx, ' to hold ', hold_idx)
-                tetherbot, commands, exitflag = self._localplanner.plan(tetherbot, grip_idx, hold_idx, commands)
+            commands: list[CommandList] = []
+            
+            # look up commands in cache
+            i = 0
+            for stance in path.stances:
                 
-                if exitflag == False:
+                stance = tuple(stance)
+                
+                if stance in command_cache:
+                    commands.append(command_cache[stance][0])
+                    tetherbot.set_state(command_cache[stance][1])
+                    i += 1
+                else:
                     break
-        
-            if exitflag == True:
+
+            for prev_stance, curr_stance in zip(path.stances[i-1:], path.stances[i:]):
+                
+                print('current state')
+                print('previous stance:     ', prev_stance)
+                print('current stance:      ', curr_stance)
+                print('platform pose:       ', tetherbot.platform.T_world.decompose())               
+                print('paltform stability:  ', tetherbot.stability())
+                print('arm state:           ', tetherbot.platform.arm.qs)
+
+                prev_stance = list(prev_stance)
+                curr_stance = list(curr_stance)
+            
+                if -1 in curr_stance:
+                    grip_idx = curr_stance.index(-1)  
+
+                    print('picking gripper: ' , grip_idx)
+                    
+                    tetherbot, commands_temp, exitflag = self._localplanner.plan(tetherbot, grip_idx, None, CommandList(), start_state = 0, goal_state = 5)
+                elif -1 in prev_stance:
+                    grip_idx = prev_stance.index(-1)
+                    hold_idx = curr_stance[grip_idx]
+
+                    print('placing gripper: ' , grip_idx, ' on hold: ', hold_idx)
+
+                    tetherbot, commands_temp, exitflag = self._localplanner.plan(tetherbot, grip_idx, hold_idx, CommandList(), start_state = 6, goal_state = 10)
+
+                if exitflag:
+                    commands.append(commands_temp)
+                    command_cache[tuple(curr_stance)] = (commands_temp, tetherbot.get_state())
+                else:
+                    self._graph.set_traversable(tuple(prev_stance), tuple(curr_stance), False)
+                    break
+
+            if exitflag:
                 print('...successful!')
-            else:
-                print('...failed.')
+                break
+            """ else: ################################# remove me
+                break """
+
+        # concatenate commands to single command list
+        commands = CommandList(sum(commands, []))        
 
         return tetherbot, commands, exitflag
 
