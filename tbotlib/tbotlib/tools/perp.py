f1b7a0f1eababfd1e1dbd8d9cc72b20fd3aef632
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2024-05-24 11:34:50 +0900

    Minor changes

--- tbotlib/tbotlib/tools/perp.py
+++ tbotlib/tbotlib/tools/perp.py
@@ -3,19 +3,24 @@ import numpy as np
 
 R = np.array([[0,1],[-1,0]])
 
-def perp(u: np.ndarray) -> np.ndarray:
+def perp(u: np.ndarray, v: np.ndarray = None) -> np.ndarray:
     '''
     Caclulate perpendicular unit vector of 2d vector u
-    u: 2D vector
+    u: 2D vector for which to calculate perpendicular unit vector
+    v: 2D vector to indicate the direction of the perpendicular unit vector, v should not be parallel to u
     '''
 
     u = R @ u
+
+    if v is not None:
+        if np.dot(u,v) < 0:
+            u *= -1
 
     return u/np.linalg.norm(u)
 
 if __name__ == '__main__':
 
     u = [-2,0]
-
-    print(perp(u))
+    v = [-1,-1]
+    print(perp(u, v))
     print(u)
