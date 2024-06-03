29495ec30fb96878c7791a7f899c846bb04ea156
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2024-06-03 11:42:00 +0900

    Renamed property _items to _values, added method values()

--- tbotlib/tbotlib/tools/Ring.py
+++ tbotlib/tbotlib/tools/Ring.py
@@ -6,13 +6,13 @@ class Ring:
     https://codereview.stackexchange.com/a/85317
     """
 
-    def __init__(self, items) -> None:
+    def __init__(self, values) -> None:
         
-        self._items = list(items)
+        self._values = list(values)
 
     def __iter__(self) -> itertools.cycle:
         
-        return itertools.cycle(self._items)
+        return itertools.cycle(self._values)
 
     def _from_integer_index(self, idx) -> int:
         
@@ -20,11 +20,11 @@ class Ring:
             
             raise TypeError("Ring indices must be integers, not {}".format(type(idx)))
 
-        if not len(self._items):
+        if not len(self._values):
             
             raise IndexError("Indexing empty ring")
 
-        return idx % len(self._items) #module
+        return idx % len(self._values) #module
 
     def __getitem__(self, idx):
         
@@ -34,28 +34,34 @@ class Ring:
             
             return [self[i] for i in range(idx.start, idx.stop, step)]
 
-        return self._items[self._from_integer_index(idx)]
+        return self._values[self._from_integer_index(idx)]
 
     def __setitem__(self, idx, value):
         
-        self._items[self._from_integer_index(idx)] = value
+        self._values[self._from_integer_index(idx)] = value
 
     def __delitem__(self, idx):
         
-        del self._items[self._from_integer_index(idx)]
+        del self._values[self._from_integer_index(idx)]
 
     def __repr__(self):
         
-        return "Ring({})".format(self._items)
+        return "Ring({})".format(self._values)
+    
+    def values(self) -> list:
 
+        return self._values
+        
     def index(self, value):
 
-        return self._items.index(value)
+        return self._values.index(value)
 
 
 if __name__ == '__main__':
 
-    r = Ring([1,2,3,4])
-    print(r)
+    r = Ring([1,2,7,4])
+    print(r.values())
     print(r[3])
     print(r[4])
+    print(r.index(2))
+    print(r._values)
