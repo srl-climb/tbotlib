2eb1f2d6f02e4aa091614bff6d02c56c6fd2ca05
s-a-i-m-o-n <harms.simon759@mail.kyutech.jp>
2024-06-07 11:07:48 +0900

    Fixed typo in class name. Renamed variables in PanorelGripperForceModel.

--- tbotlib/tbotlib/models/GripperForceModel.py
+++ tbotlib/tbotlib/models/GripperForceModel.py
@@ -18,28 +18,32 @@ class GripperForceModel:
         thetas = np.linspace(0, 180, 100)
         fs = self.eval(thetas)
 
-        plt.plot(thetas, fs)
-        plt.ylim(bottom = -1, top=40)
+        np.float = float
+        fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+        ax.set_theta_zero_location("N")
+        ax.set_theta_direction(-1)
+        ax.plot(np.deg2rad(thetas), fs)
+        ax.set_ylim(bottom = -1, top=40)
         plt.show()
     
 
-class PanorelGripperFoceModel(GripperForceModel):
+class PanorelGripperForceModel(GripperForceModel):
 
-    def __init__(self, kappa_1: float = 18.0, kappa_2: float = 28.0) -> None:
+    def __init__(self, fh_max: float = 18.0, fv_max: float = 28.0) -> None:
         
-        self.kappa_1 = kappa_1
-        self.kappa_2 = kappa_2
+        self.fh_max = fh_max
+        self.fv_max = fv_max
         
     def eval(self, thetas: np.ndarray) -> float:
         
         thetas = np.deg2rad(90 - thetas)
         
         h = thetas>=0
-        f = np.hstack((np.minimum(self.kappa_1 / np.cos(thetas[h]), self.kappa_2 / np.sin(thetas[h])) , self.kappa_1 / np.cos(thetas[np.logical_not(h)])))
+        f = np.hstack((np.minimum(self.fh_max / np.cos(thetas[h]), self.fv_max / np.sin(thetas[h])) , self.fh_max / np.cos(thetas[np.logical_not(h)])))
 
         return f 
     
 if __name__ == '__main__':
 
-    model = PanorelGripperFoceModel()
+    model = PanorelGripperForceModel()
     model.debug_plot()
